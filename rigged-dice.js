7e07914947dd388d3ad0052b37aa54f3316a7a0c
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-10 08:28:32 -0600

    update

--- rigged-dice.js
+++ rigged-dice.js
@@ -1,10 +1,35 @@
+// const prompt = require('prompt-sync')();
+
+// let sides = Number(prompt("How many sides does your die have? ")); //probability is 1/sides
+// let rigged = Number(prompt("What side is loaded? "));              //desire to have a probability of 2/sides
+// let twice = 2 / sides;
+
+// // if (sides === 0 || sides === 1) {
+// //     console.log("Impossible.")
+// // } else if (sides >= 2){
+// //     console.log(((Math.ceil(Math.random()) * sides)) * result);
+// // }
+
+// if ()
+
+// console.log((Math.ceil(Math.random()*sides)) * (twice * rigged));
+
+
+
+
+
+
+
+
+
 const prompt = require('prompt-sync')();
 
-let roll = Number(prompt("How many sides does your die have? "));
-let loaded = Number(prompt("What side is loaded? "));
+let sides = Number(prompt("How many sides does your die have? ")); 
+let prob1 = 1 / sides;
 
-if (roll === ((1 || 2 || 3 || 4 || 5 || 6 ) && (roll === loaded))) {
-    console.log((Math.ceil(Math.random()*(roll))) * (Math.ceil(Math.pow(1/loaded,2))));
-} else {
-    
-}
+let rigged = Number(prompt("What side is loaded? "));              
+let prob2 = rigged * (2 / sides);
+
+let random = ((Math.ceil(Math.random())) * sides);
+
+console.log(random * prob1) + ()
